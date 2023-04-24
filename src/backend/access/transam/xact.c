2772f8dccf66819d785b9cb25f9b45e3210818d7
Tejasvi Kashi <pct960@gmail.com>
2023-04-24 12:31:52 -0400

    Added xlogflush when sync_standbys are absent

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -1426,51 +1426,56 @@ RecordTransactionCommit(void)
 		 */
 		if (!wrote_xlog && synchronous_commit > SYNCHRONOUS_COMMIT_OFF)
 		{
-			//XLogRecPtr XLogMaxLSN = XLogGetMaxLSN(NULL);
-			//maxLSN = XLogGetMaxLSN(NULL);
-			//XLogRecPtr walSndAppliedLSN = SyncRepGetWalSndLSN();
-			//XLogRecPtr RecentFlushPtr = InvalidXLogRecPtr;
-			//if (!RecoveryInProgress())
-			//	RecentFlushPtr = GetFlushRecPtr(NULL);
-			//else
-			//	RecentFlushPtr = GetXLogReplayRecPtr(NULL);
+			if(!((volatile WalSndCtlData *) WalSndCtl)->sync_standbys_defined)
+				XLogFlush(maxLSN);
+			else
+			{
+				//XLogRecPtr XLogMaxLSN = XLogGetMaxLSN(NULL);
+				//maxLSN = XLogGetMaxLSN(NULL);
+				//XLogRecPtr walSndAppliedLSN = SyncRepGetWalSndLSN();
+				//XLogRecPtr RecentFlushPtr = InvalidXLogRecPtr;
+				//if (!RecoveryInProgress())
+				//	RecentFlushPtr = GetFlushRecPtr(NULL);
+				//else
+				//	RecentFlushPtr = GetXLogReplayRecPtr(NULL);
 
-			//LWLockAcquire(SyncRepLock, LW_EXCLUSIVE);
-			//PGPROC *proc; 
-			//proc = (PGPROC *) SHMQueueNext(&(WalSndCtl->SyncRepQueue[synchronous_commit]),
-			//					   &(WalSndCtl->SyncRepQueue[synchronous_commit]),
-			//					   offsetof(PGPROC, syncRepLinks));
+				//LWLockAcquire(SyncRepLock, LW_EXCLUSIVE);
+				//PGPROC *proc; 
+				//proc = (PGPROC *) SHMQueueNext(&(WalSndCtl->SyncRepQueue[synchronous_commit]),
+				//					   &(WalSndCtl->SyncRepQueue[synchronous_commit]),
+				//					   offsetof(PGPROC, syncRepLinks));
 			
-			//if(proc)
-			//	elog(INFO, "queue not empty");
-			//else
-			//	elog(INFO, "queue empty");
+				//if(proc)
+				//	elog(INFO, "queue not empty");
+				//else
+				//	elog(INFO, "queue empty");
 
-			//bool queueEmpty = SHMQueueEmpty(&(WalSndCtl->SyncRepQueue[Min(synchronous_commit, SYNC_REP_WAIT_APPLY)]));
-			//LWLockRelease(SyncRepLock);
+				//bool queueEmpty = SHMQueueEmpty(&(WalSndCtl->SyncRepQueue[Min(synchronous_commit, SYNC_REP_WAIT_APPLY)]));
+				//LWLockRelease(SyncRepLock);
 
-			XLogRecPtr remoteFlushLSN = ((volatile WalSndCtlData *) WalSndCtl)->lsn[Min(synchronous_commit, SYNC_REP_WAIT_APPLY)];
-			//XLogRecPtr minSentLSN = getMinSentLSN();
-			//XLogRecPtr maxSnapshotLSN = getMaxLSNFromSnapshot(); 
-			//XLogRecPtr maxSnapshotLSN = TransactionIdGetCommitLSN(MyProc->xmin);
+				XLogRecPtr remoteFlushLSN = ((volatile WalSndCtlData *) WalSndCtl)->lsn[Min(synchronous_commit, SYNC_REP_WAIT_APPLY)];
+				//XLogRecPtr minSentLSN = getMinSentLSN();
+				//XLogRecPtr maxSnapshotLSN = getMaxLSNFromSnapshot(); 
+				//XLogRecPtr maxSnapshotLSN = TransactionIdGetCommitLSN(MyProc->xmin);
 
-			//elog(INFO, "insert_rec_ptr = (%d), last_important_ptr = (%d)", GetInsertRecPtr(), GetLastImportantRecPtr());
+				//elog(INFO, "insert_rec_ptr = (%d), last_important_ptr = (%d)", GetInsertRecPtr(), GetLastImportantRecPtr());
 
-			// EDXXX: If remoteflushlsn = 0, then a read only transaction may not be able to decide if it has to add itself
-			// to the queue. Why? a write txn may have finished committing and becoming visible, but may not have enqueued itself
-			// in the syncrep queue. 
+				// EDXXX: If remoteflushlsn = 0, then a read only transaction may not be able to decide if it has to add itself
+				// to the queue. Why? a write txn may have finished committing and becoming visible, but may not have enqueued itself
+				// in the syncrep queue. 
 
-			// To solve this issue. Here is a potential strategy that might work
-			// 1. 	Refer to pgstatfuncs.c to scan running backends.
-			// 		if a backend is running and has an xid, then it is a RW txn - so it's safe for the RO txn to add itself
-			// 		to the queue irrespective of which sync level it is waiting for (syncrepwakequeue will wake up all procs in all queues)
-			// 2.	If a backend is running and does not have an xid, then check its wait_event to see if it is in 'SyncRepWait' mode.
-			//		if yes, then it is safe for the RO txn to add itself to the queue.
+				// To solve this issue. Here is a potential strategy that might work
+				// 1. 	Refer to pgstatfuncs.c to scan running backends.
+				// 		if a backend is running and has an xid, then it is a RW txn - so it's safe for the RO txn to add itself
+				// 		to the queue irrespective of which sync level it is waiting for (syncrepwakequeue will wake up all procs in all queues)
+				// 2.	If a backend is running and does not have an xid, then check its wait_event to see if it is in 'SyncRepWait' mode.
+				//		if yes, then it is safe for the RO txn to add itself to the queue.
 
-			// EDXXX: The next todo is to only wait for the highest commit lsn we have seen, and not for the 
-			// the highest lsn in the xlog. Yet to think of a correct way of doing this.
-			if(maxLSN > remoteFlushLSN && remoteFlushLSN > 0)
-				SyncRepWaitForLSN(maxLSN, false);
+				// EDXXX: The next todo is to only wait for the highest commit lsn we have seen, and not for the 
+				// the highest lsn in the xlog. Yet to think of a correct way of doing this.
+				if(maxLSN > remoteFlushLSN && remoteFlushLSN > 0)
+					SyncRepWaitForLSN(maxLSN, false);
+			}
 		}
 		if (!wrote_xlog)
 			goto cleanup;
