a055d95e3dae09cbbd6d7a68f91cc6fcd64a04be
Tejasvi Kashi <pct960@gmail.com>
2024-02-25 15:41:54 -0500

    Updated init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,7 @@
-create table data (key serial primary key not null, value int);
-insert into data(value) values(generate_series(1, 1000000));
+create table data (key int primary key, value int);
 
+-- Use this for 'durability-costs' and 'ed-txn-latency' tests
+insert into data values(generate_series(1, 1000000), generate_series(1, 1000000));
+
+-- Use this for contention tests
+--insert into data values(generate_series(1, 250), generate_series(1, 250));
