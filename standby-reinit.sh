bb8f5af7de6e1b2a6bd344d5be7b282ee528537c
Ubuntu <ubuntu@ip-10-0-1-112.ec2.internal>
2024-09-16 17:06:20 +0000

    More config changes

--- standby-reinit.sh
+++ standby-reinit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#sudo pkill -9 postgres
+sudo pkill -9 postgres
 rm -rf node-2
-pg_basebackup -h localhost -U postgres -p 5432 -D node-2 -Fp -Xs -P -R
+pg_basebackup -h 10.0.1.35 -U postgres -p 5432 -D node-2 -Fp -Xs -P -R
 #sed -i '/#hot_standby/s/^#//g' node-2/postgresql.conf
 #sed -i -e "/cluster_name =/ s/= .*/= \\'replica1\\'/" node-2/postgresql.conf
 #sed -i -e "/default_transaction_isolation =/ s/= .*/= \\'read committed\\'/" node-2/postgresql.conf
